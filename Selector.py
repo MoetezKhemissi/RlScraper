2086b7347c97411cd8fcf8750ba2dc083be7296f
MoetezKhemissi <Khemissimoetez@gmail.com>
2022-12-24 02:52:40 +0100

    Selector

--- Selector.py
+++ Selector.py
@@ -14,31 +14,51 @@ from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime
 import time
 import utils
-driver = webdriver.Chrome()
-driver.get("https://rocket-league.com/trading")
-Button =  driver.find_element(By.ID, "acceptPrivacyPolicy")
-# click it
+def fresh_start(driver):
+    driver.get("https://rocket-league.com/trading")
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    Button =  driver.find_element(By.ID, "acceptPrivacyPolicy")
+    Button.click()
+    return current_time
+def get_fields_select(driver,name):
 
-# lazy timing TODO get server time for synchro
-Button.click()
-#with specific value
+    path_to_fields="rlg-itemfilter__"+name
+    Selected_options = driver.find_element(By.CLASS_NAME, path_to_fields)
+    Selected_options.click()
+    list = Selected_options.find_element(By.CLASS_NAME, "chosen-results")
+    final_list = list.find_elements(By.TAG_NAME, "li")
+    return final_list
+
+
+#Possible select_from values:
+#   "item"
+#   "paint"
+#   "platform"
+def select_field(driver,select_from,value_to_be_selected):
+    fields = get_fields_select(driver,select_from)
+    for element in fields:
+        if(element.text==value_to_be_selected):
+            element.click()
+
+def get_all_possible_values(driver,select_from):
+    values=[]
+    fields = get_fields_select(driver,select_from)
+    for element in fields:
+        values.append(element.text)
+    return values
+def get_possible_values_and_export_csv(driver,select_from):
+    utils.json_write(select_from,get_all_possible_values(driver,select_from))
+
+items=[]
 test_with_string="All Decals"
-Selected_options = driver.find_element(By.CLASS_NAME, "rlg-itemfilter__item")
-Selected_options.click()
-Inside_div =Selected_options.find_element(By.TAG_NAME, "div")
-inside_inside_div=Selected_options.find_element(By.TAG_NAME, "div")
-list = Selected_options.find_element(By.TAG_NAME, "ul")
-final_list = Selected_options.find_elements(By.TAG_NAME, "li")
-for element in final_list:
-    if(element.text==test_with_string):
-        print(element.text)
-        element.click()
-#Selected_fields = driver.find_element(By.CLASS_NAME, "rlg-itemfilter__item").find_element(By.CLASS_NAME, "chosen-with-drop").find_element(By.CLASS_NAME, "chosen-drop").find_element(By.CLASS_NAME, "chosen-drop").find_elements(By.CLASS_NAME, "active-result")
-#for field in Selected_fields:
-#    if field.text == test_with_string:
-#        print(test_with_string)
-#        field.click()
-time.sleep(10)
+name="item"
+driver = webdriver.Chrome()
+fresh_start(driver)
+get_possible_values_and_export_csv(driver,"platform")
+
 driver.close()
-utils.json_write("items",)
 # todo get item list paint list platform
+
+def search_list_export_pdf():
+    pass
